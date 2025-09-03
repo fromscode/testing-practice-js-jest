424deddbbcd45533710cad95a4c049cc94df89ce
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-09-03 16:14:29 +0000

    Create capitalize function

---
+++ capitalize.test.js
@@ -0,0 +1,13 @@
+import capitalize from "./capitalize.js";
+
+test("capitalizes hello world", () => {
+  expect(capitalize("hello world")).toBe("Hello world");
+});
+
+test("capitalizes bond, james bond", () => {
+  expect(capitalize("bond, james bond")).toBe("Bond, james bond");
+});
+
+test("capitalizes que mira bobo", () => {
+  expect(capitalize("que mira bobo")).toBe("Que mira bobo");
+});
